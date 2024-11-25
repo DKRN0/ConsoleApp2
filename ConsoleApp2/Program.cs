1349f79b080e9061192fdf6def6cf818e11c7426
DKRN0 <islammazitov03@gmail.com>
2024-11-25 16:29:34 +0300

    1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,3 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("S Круга = " + Math.PI * Math.Pow(25, 2));
+Console.WriteLine("S Треугольника =" +(15*9)/2);
+Console.WriteLine("S Прямоугольника =" + 15 * 200); 
+Console.WriteLine("До закрытия программы осталось 10 секунд");
+await (Task.Delay(10000));
 
-Console.WriteLine("Hello, World!");
--- ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net9.0/ConsoleApp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d8f6c1fb8191a595e6b2c998823fe455b7236a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
